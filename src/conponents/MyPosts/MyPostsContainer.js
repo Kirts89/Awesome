a84e5406a75b1653fbe37959c74990ec417a799f
Kirts <kirtsden89@gmail.com>
2021-06-08 14:56:45 +0300

    last save

--- src/conponents/MyPosts/MyPostsContainer.js
+++ src/conponents/MyPosts/MyPostsContainer.js
@@ -4,13 +4,9 @@ import MyPosts from "./MyPosts";
 import {setMyPosts, setMyPostsIsFetching} from "../../redux/myPostsReducer";
 import Loading from "../Loading/Loading";
 import {PostsAPI} from "../../api/api";
-import axios from "axios";
 
 class MyPostsContainer extends React.Component {
   componentDidMount() {
-    //this.props.setMyPostsIsFetching()
-    // PostsAPI.getAll('posts').then((data) => {
-    //this.props.setMyPosts(data)})
     this.props.setMyPostsIsFetching()
     PostsAPI.getAll().then((data) => {
       this.props.setMyPosts(data)
